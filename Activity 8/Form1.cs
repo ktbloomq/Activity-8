41545adc7ed2a6c1a7ed0af54734ce087febdab5
Kevin <ktb@bloomquist.com>
2022-10-05 14:12:23 -0700

    Add project files.

---
+++ Activity 8/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double FatCalories(double fat)
+        {
+            return fat * 9;
+        }
+
+        public double CarbCalories(double carbs)
+        {
+            return carbs * 4;
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            double fat, carbs, calories;
+            if(double.TryParse(fatInput.Text, out fat) && double.TryParse(carbInput.Text, out carbs)) {
+                calories = FatCalories(fat) + CarbCalories(carbs);
+                MessageBox.Show("Your calorie intake is " + calories);
+            } else
+            {
+                MessageBox.Show("Input Error");
+            }
+        }
+    }
+}
